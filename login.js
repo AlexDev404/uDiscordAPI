18e07ad517ed459305130f013b7377f4115cc163
Immanuel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-11-09 10:33:10 -0600

    Update login.js

--- login.js
+++ login.js
@@ -10,10 +10,8 @@ fetch("https://discord.com/api/v9/auth/login", {
     "content-type": "application/json",
   },
 
-  "body": `{\"login\":\"${email}\",\"${pass}\":\"@lexgArc1a101105\",\"undelete\":false,\"captcha_key\":null,\"login_source\":null,\"gift_code_sku_id\":null}`,
+  "body": `{\"login\":\"${email}\",\"password\":\"${pass}\",\"undelete\":false,\"captcha_key\":null,\"login_source\":null,\"gift_code_sku_id\":null}`,
   "method": "POST",
   "mode": "cors",
   "credentials": "include"
 }).then((res)=>res.json()).then((json)=>token=json);
-
-console.log(`CLIENT TOKEN: ${token}`);
